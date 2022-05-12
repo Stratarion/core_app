4eec6b0a9e77e85776c6c18e57149e2c567c31fa
volkov <volkov.oleg@otr.ru>
2022-05-12 15:04:33 +0300

    Перевод на ант страницы авторизации

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,42 +1,29 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { Row, Col, Form, Input, Button, Card } from 'antd';
 import { useHistory } from 'react-router-dom';
 import { GoogleLogin } from 'react-google-login';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
-import Icon from './icon';
-import { signin, signup, googlesignin } from '../../actions/auth';
+import { signup, signin, googlesignin } from '../../actions/auth';
 // import { AUTH } from '../../constants/actionTypes';
-import useStyles from './styles';
-import Input from './Input';
-
-// const test = true;
-
-const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
 
 const SignUp = () => {
-  const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
-  const classes = useStyles();
-
-  const [showPassword, setShowPassword] = useState(false);
-  const handleShowPassword = () => setShowPassword(!showPassword);
 
   const switchMode = () => {
-    setForm(initialState);
     setIsSignup((prevIsSignup) => !prevIsSignup);
-    setShowPassword(false);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    console.log(form);
-
-    // if (test) return;
+  const handleSubmit = (values) => {
+    const form = signin ? { ...values }
+      : {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        password: values.password,
+        email: values.email,
+      };
 
     if (isSignup) {
       dispatch(signup(form, history));
@@ -53,51 +40,90 @@ const SignUp = () => {
 
   const googleError = () => console.log('Google Sign In was unsuccessful. Try again later');
 
-  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  const renderSignUp = () => (
+    <>
+      <Col span={24}>
+        <Form.Item
+          name="firstName"
+          label="First Name"
+        >
+          <Input placeholder="Имя" />
+        </Form.Item>
+      </Col>
+      <Col span={24}>
+        <Form.Item
+          name="lastName"
+          label="Last Name"
+        >
+          <Input placeholder="Фамилия" />
+        </Form.Item>
+      </Col>
+    </>
+  );
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Paper className={classes.paper} elevation={6}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            { isSignup && (
-            <>
-              <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-              <Input name="lastName" label="Last Name" handleChange={handleChange} half />
-            </>
-            )}
-            <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
-            <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-            { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
-          </Grid>
-          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-            { isSignup ? 'Sign Up' : 'Sign In' }
-          </Button>
-          <GoogleLogin
-            clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
-            render={(renderProps) => (
-              <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
-                Google Sign In
-              </Button>
-            )}
-            onSuccess={googleSuccess}
-            onFailure={googleError}
-            cookiePolicy="single_host_origin"
-          />
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Button onClick={switchMode}>
-                { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Paper>
-    </Container>
+    <Row>
+      <Col offset={8} span={8}>
+        <Card title={isSignup ? 'Sign up' : 'Sign in'}>
+          <Form
+            layout="vertical"
+            hideRequiredMark
+            onFinish={handleSubmit}
+          >
+            <Row gutter={[16, 16]}>
+              { isSignup && renderSignUp()}
+              <Col span={24}>
+                <Form.Item
+                  name="email"
+                  label="Email Address"
+                >
+                  <Input placeholder="Email Address" />
+                </Form.Item>
+              </Col>
+              <Col span={24}>
+                <Form.Item
+                  name="password"
+                  label="Password"
+                >
+                  <Input.Password placeholder="input password" />
+                </Form.Item>
+              </Col>
+              { isSignup && (
+                <Col span={24}>
+                  <Form.Item
+                    name="confirmPassword"
+                    label="Repeat Password"
+                  >
+                    <Input.Password placeholder="Repeat password" />
+                  </Form.Item>
+                </Col>
+              )}
+              <Col>
+                <Button htmlType="submit">{ isSignup ? 'Sign Up' : 'Sign In' }</Button>
+              </Col>
+              <Col>
+                <GoogleLogin
+                  clientId="564033717568-bu2nr1l9h31bhk9bff4pqbenvvoju3oq.apps.googleusercontent.com"
+                  render={(renderProps) => (
+                    <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                      Google Sign In
+                    </Button>
+                  )}
+                  onSuccess={googleSuccess}
+                  onFailure={googleError}
+                  cookiePolicy="single_host_origin"
+                />
+              </Col>
+              <Col>
+                <Button onClick={switchMode}>
+                  { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
