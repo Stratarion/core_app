9b4e44a0b0bfb2d19d3dd59a007ed164491ac038
volkov <volkov.oleg@otr.ru>
2022-05-12 16:07:06 +0300

    чистка

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -5,9 +5,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from '../../actions/posts';
 
 const { Text, Title } = Typography;
-// import { Drawer, Form as AntForm, Button as AntButton, Col, Row, Input, Select, DatePicker, Space } from 'antd';
-// import { PlusOutlined } from '@ant-design/icons';
-// const { Option } = Select;
 
 const Form = ({ currentId, setCurrentId, showForm }) => {
   const post = useSelector((state) => (currentId ? state.posts.posts.find((message) => message._id === currentId) : null));
@@ -48,14 +45,6 @@ const Form = ({ currentId, setCurrentId, showForm }) => {
     );
   }
 
-  // const handleAddChip = (tag) => {
-  //   setPostData({ ...postData, tags: [...postData.tags, tag] });
-  // };
-
-  // const handleDeleteChip = (chipToDelete) => {
-  //   setPostData({ ...postData, tags: postData.tags.filter((tag) => tag !== chipToDelete) });
-  // };
-
   return (
     <>
       <AForm onFinish={handleSubmit}>
@@ -92,26 +81,6 @@ const Form = ({ currentId, setCurrentId, showForm }) => {
           </Col>
         </Row>
       </AForm>
-      {/* <Paper className={classes.paper} elevation={6}>
-        <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-          <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-          <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-          <div style={{ padding: '5px 0', width: '94%' }}>
-            <ChipInput
-              name="tags"
-              variant="outlined"
-              label="Tags"
-              fullWidth
-              value={postData.tags}
-              onAdd={(chip) => handleAddChip(chip)}
-              onDelete={(chip) => handleDeleteChip(chip)}
-            />
-          </div>
-          <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
-          <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-          <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
-        </form>
-      </Paper> */}
     </>
   );
 };
