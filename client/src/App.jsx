12f2c9274c8f4611bbd391e838ef041344824c2e
volkov <volkov.oleg@otr.ru>
2022-05-18 21:39:12 +0300

    Вынес роутер

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,25 @@
 import React from 'react';
 import { Layout } from 'antd';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import {
-  PostDetails,
-  PostsPage,
-  AuthPage,
-  CreatorOrTag,
-  PageTimeshit,
-  PageProfessionals,
-} from 'pages';
+import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from 'components';
-import ROUTS from 'router/routs';
 import { MainLib } from 'lib';
+import Router from 'router';
 
 const { Header, Content, Footer } = Layout;
 const contentStyle = { padding: '20px 50px' };
 
-const App = () => {
-  const user = JSON.parse(localStorage.getItem('profile'));
-
-  return (
-    <BrowserRouter>
-      <Layout className="layout">
-        <Header>
-          <Navbar />
-        </Header>
-        <Content style={contentStyle}>
-          <Switch>
-            <Route path={ROUTS.HOME} exact component={() => <Redirect to={ROUTS.POSTS} />} />
-            <Route path={ROUTS.POSTS} exact component={PostsPage} />
-            <Route path={`${ROUTS.POSTS}/search`} exact component={PostsPage} />
-            <Route path={`${ROUTS.POSTS}/:id`} exact component={PostDetails} />
-            <Route path={[`${ROUTS.CREATORS}/:name`, `${ROUTS.TAGS}/:name`]} component={CreatorOrTag} />
-            <Route path={ROUTS.AUTH} exact component={() => (!user ? <AuthPage /> : <Redirect to={ROUTS.POSTS} />)} />
-            <Route path={ROUTS.TIMESHIT} exact component={PageTimeshit} />
-            <Route path={ROUTS.PROFESSIONALS} exact component={PageProfessionals} />
-          </Switch>
-        </Content>
-        <Footer style={{ textAlign: 'center' }}>{MainLib.footerText}</Footer>
-      </Layout>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Layout className="layout">
+      <Header>
+        <Navbar />
+      </Header>
+      <Content style={contentStyle}>
+        <Router />
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>{MainLib.footerText}</Footer>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default App;
